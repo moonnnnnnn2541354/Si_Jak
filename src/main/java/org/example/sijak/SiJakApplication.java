6a52fcd59c3983efe8f6d34ad7054bb5fcb16fed
dong <ioae7979@gmail.com>
2024-03-08 11:47:41 +0900

    first commit

---
+++ src/main/java/org/example/sijak/SiJakApplication.java
@@ -0,0 +1,13 @@
+package org.example.sijak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiJakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiJakApplication.class, args);
+	}
+
+}
