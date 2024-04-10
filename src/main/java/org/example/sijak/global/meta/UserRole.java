499205fff033df7f7925711214ed92816d161d00
dong <ioae7979@gmail.com>
2024-04-10 22:21:26 +0900

    feat: UserRole

---
+++ src/main/java/org/example/sijak/global/meta/UserRole.java
@@ -0,0 +1,21 @@
+package org.example.sijak.global.meta;
+
+public enum UserRole {
+    USER(Authority.USER),
+    ADMIN(Authority.ADMIN);
+    private final String authority;
+
+    UserRole(String authority) {
+        this.authority = authority;
+    }
+
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public static class Authority {
+        public static final String USER = "USER";
+        public static final String ADMIN = "ADMIN";
+    }
+
+}
